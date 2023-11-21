7bb16b5426692f98446bb8cfe8185844e4770592
S M <hubble.stein@gmail.com>
2023-11-22 06:16:36 +0900

    reduced nested if

--- console_tetris/console_tetris.cpp
+++ console_tetris/console_tetris.cpp
@@ -9,202 +9,196 @@ import Tetrominoes;
 
 std::unique_ptr<Tetromino> getRandomTetromino()
 {
-	std::unique_ptr<Tetromino> tobj{};
+    std::unique_ptr<Tetromino> tObj{};
 
-	int num{ randomTetromino() };
-	if (num == 1)
-		tobj = std::make_unique<TetrominoZ>(4, 0);
-	else if (num == 2)
-		tobj = std::make_unique<TetrominoS>(4, 0);
-	else if (num == 3)
-		tobj = std::make_unique<TetrominoO>(4, 0);
-	else if (num == 4)
-		tobj = std::make_unique<TetrominoT>(4, 0);
-	else if (num == 5)
-		tobj = std::make_unique<TetrominoI>(4, 0);
-	else if (num == 6)
-		tobj = std::make_unique<TetrominoJ>(4, 0);
-	else
-		tobj = std::make_unique<TetrominoL>(4, 0);
+    int num{ randomTetromino() };
+    if (num == 1)
+        tObj = std::make_unique<TetrominoZ>(4, 0);
+    else if (num == 2)
+        tObj = std::make_unique<TetrominoS>(4, 0);
+    else if (num == 3)
+        tObj = std::make_unique<TetrominoO>(4, 0);
+    else if (num == 4)
+        tObj = std::make_unique<TetrominoT>(4, 0);
+    else if (num == 5)
+        tObj = std::make_unique<TetrominoI>(4, 0);
+    else if (num == 6)
+        tObj = std::make_unique<TetrominoJ>(4, 0);
+    else
+        tObj = std::make_unique<TetrominoL>(4, 0);
 
-	return tobj;
+    return tObj;
 }
 
 // freeze the current tetromino and create a new one
 // if new one fits the grid then continue the game(return true)
 // otherwise finish the game(return false)
-bool freezeTetromino(std::unique_ptr<Tetromino>& tobj, std::unique_ptr<Tetromino>& tNext, Grid& grid)
+bool freezeTetromino(std::unique_ptr<Tetromino>& tObj, std::unique_ptr<Tetromino>& tNext, Grid& grid)
 {
-	tobj->drop(grid);
-	tobj->updateGrid(grid);
-	grid.draw();
-	grid.deleteFilledRows();
+    tObj->drop(grid);
+    tObj->updateGrid(grid);
+    grid.draw();
+    grid.deleteFilledRows();
 
 
-	tNext->setX(tNext->getX() - 15);
+    tNext->setX(tNext->getX() - 15);
 
-	if (tNext->isClear(grid))
-	{
-		tNext->setX(tNext->getX() + 15);
-		tNext->erase();
+    if (tNext->isClear(grid))
+    {
+        tNext->setX(tNext->getX() + 15);
+        tNext->erase();
 
-		tobj = std::move(tNext);
-		tobj->setX(tobj->getX() - 15);
-		tNext = getRandomTetromino();
-		tNext->setX(tNext->getX() + 15);
+        tObj = std::move(tNext);
+        tObj->setX(tObj->getX() - 15);
+        tNext = getRandomTetromino();
+        tNext->setX(tNext->getX() + 15);
 
-		tobj->draw();
-		tNext->draw();
-		return true;
-	}
+        tObj->draw();
+        tNext->draw();
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 void clearKeyState()
 {
-	//clear last escape and arrow keys
-	GetAsyncKeyState(VK_ESCAPE);
-	GetAsyncKeyState(VK_UP);
-	GetAsyncKeyState(VK_LEFT);
-	GetAsyncKeyState(VK_RIGHT);
-	GetAsyncKeyState(VK_SPACE);
-	GetAsyncKeyState(VK_DOWN);
-	GetAsyncKeyState(0x50); // P Key
+    //clear last escape and arrow keys
+    GetAsyncKeyState(VK_ESCAPE);
+    GetAsyncKeyState(VK_UP);
+    GetAsyncKeyState(VK_LEFT);
+    GetAsyncKeyState(VK_RIGHT);
+    GetAsyncKeyState(VK_SPACE);
+    GetAsyncKeyState(VK_DOWN);
+    GetAsyncKeyState(0x50); // P Key
 }
 
 // adjust console size to draw border and etc.
 void adjustConsoleSize()
 {
-	CONSOLE_SCREEN_BUFFER_INFOEX consolesize;
+    CONSOLE_SCREEN_BUFFER_INFOEX consolesize;
 
-	consolesize.cbSize = sizeof(consolesize);
+    consolesize.cbSize = sizeof(consolesize);
 
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	GetConsoleScreenBufferInfoEx(hConsole, &consolesize);
+    GetConsoleScreenBufferInfoEx(hConsole, &consolesize);
 
-	COORD c;
-	c.X = 80;
-	c.Y = 40;
-	consolesize.dwSize = c;
+    COORD c;
+    c.X = 80;
+    c.Y = 40;
+    consolesize.dwSize = c;
 
-	consolesize.srWindow.Left = 0;
-	consolesize.srWindow.Right = 80;
-	consolesize.srWindow.Top = 0;
-	consolesize.srWindow.Bottom = 40;
+    consolesize.srWindow.Left = 0;
+    consolesize.srWindow.Right = 80;
+    consolesize.srWindow.Top = 0;
+    consolesize.srWindow.Bottom = 40;
 
-	SetConsoleScreenBufferInfoEx(hConsole, &consolesize);
+    SetConsoleScreenBufferInfoEx(hConsole, &consolesize);
 }
 
 int main()
 {
-	auto myWindow{ GetForegroundWindow() };
-	
-	adjustConsoleSize();
-	 
-	// adjust console size option #2
-	/*HWND console = GetConsoleWindow();
-	RECT ConsoleRect;
-	GetWindowRect(console, &ConsoleRect);
-	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 800, 600, TRUE);*/
+    auto myWindow{ GetForegroundWindow() };
 
-	system("chcp 65001");
-	system("CLS");
+    adjustConsoleSize();
 
-	printBorder();
-	Grid grid{}; //initialized with all false, which means not occupied
+    // adjust console size option #2
+    /*HWND console = GetConsoleWindow();
+    RECT ConsoleRect;
+    GetWindowRect(console, &ConsoleRect);
+    MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 800, 600, TRUE);*/
+
+    system("chcp 65001");
+    system("CLS");
+
+    printBorder();
+    Grid grid{}; //initialized with all false, which means not occupied
 
 
-	std::unique_ptr<Tetromino> tobj{ getRandomTetromino()};
-	std::unique_ptr<Tetromino> tNext{ getRandomTetromino() };
+    auto tObj{ getRandomTetromino() };
+    auto tNext{ getRandomTetromino() };
 
-	clearKeyState();
+    clearKeyState();
 
-	tobj->draw();
-	tNext->setX(tNext->getX() + 15); // move 15 steps to the right and draw it
-	tNext->draw();
+    tObj->draw();
+    tNext->setX(tNext->getX() + 15); // move 15 steps to the right and draw it
+    tNext->draw();
 
-	int count{};
-	bool exitGame{};
-	bool pause{};
-	HWND fore{ GetForegroundWindow()};
-	while (true)
-	{
-		if (myWindow == fore)
-		{
-			if (GetAsyncKeyState(VK_ESCAPE) & 0x01)
-			{
-				break;
-			}
-			else if (GetAsyncKeyState(0x50) & 0x01) // P Key
-			{
-				pause = !pause;
-				if (pause)
-				{
-					gotoxy(1, -1);
-					std::print("{}", "PAUSED");
-				}
-				else
-				{
-					gotoxy(1, -1);
-					std::print("{}", "      ");
-				}
-			}
-			else if (GetAsyncKeyState(VK_UP) & 0x01)
-			{
-				if (!pause) tobj->rotate(grid);
-			}
-			else if (GetAsyncKeyState(VK_LEFT) & 0x01)
-			{
-				if (!pause) tobj->moveLeft(grid);
-			}
-			else if (GetAsyncKeyState(VK_RIGHT) & 0x01)
-			{
-				if (!pause) tobj->moveRight(grid);
-			}
-			else if (GetAsyncKeyState(VK_DOWN) & 0x01)
-			{
-				if (!pause && !tobj->moveDown(grid))
-					if (!freezeTetromino(tobj, tNext, grid))
-					{
-						exitGame = true;
-						break;
-					}
-			}
-			else if (GetAsyncKeyState(VK_SPACE) & 0x01)
-			{
-				if (!pause && !freezeTetromino(tobj, tNext, grid))
-				{
-					exitGame = true;
-					break;
-				}
-			}
-		} //keyboard
+    int count{};
+    bool exitGame{};
+    bool pause{};
+    HWND fore{ GetForegroundWindow() };
+    while (true)
+    {
+        if (myWindow == fore)
+        {
+            if (GetAsyncKeyState(VK_ESCAPE) & 0x01)
+            {
+                break;
+            }
+            else if (GetAsyncKeyState(0x50) & 0x01) // P Key
+            {
+                pause = !pause;
+                if (pause)
+                {
+                    gotoxy(1, -1);
+                    std::print("{}", "PAUSED");
+                }
+                else
+                {
+                    gotoxy(1, -1);
+                    std::print("{}", "      ");
+                }
+            }
+            else if (!pause && (GetAsyncKeyState(VK_UP) & 0x01))
+            {
+                tObj->rotate(grid);
+            }
+            else if (!pause && (GetAsyncKeyState(VK_LEFT) & 0x01))
+            {
+                tObj->moveLeft(grid);
+            }
+            else if (!pause && (GetAsyncKeyState(VK_RIGHT) & 0x01))
+            {
+                tObj->moveRight(grid);
+            }
+            else if (!pause && (GetAsyncKeyState(VK_DOWN) & 0x01)
+                && !tObj->moveDown(grid) && !freezeTetromino(tObj, tNext, grid))
+            {
+                exitGame = true;
+                break;
+            }
+            else if (!pause && (GetAsyncKeyState(VK_SPACE) & 0x01)
+                && !freezeTetromino(tObj, tNext, grid))
+            {
+                exitGame = true;
+                break;
+            }
+        } //keyboard
 
-		gotoxy(0, 0);
-		std::this_thread::sleep_for(std::chrono::milliseconds(50)); //otherwise, cpu usage would be high	s
-		if (!pause)
-		{
-			++count;
-			if (count == 20)
-			{
-				count = 0;
-				if (!tobj->moveDown(grid))
-					if (!freezeTetromino(tobj, tNext, grid))
-					{
-						exitGame = true;
-						break;
-					}
-			}
-		}
-		fore = GetForegroundWindow();
-	}  // while
+        gotoxy(0, 0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50)); //otherwise, cpu usage would be high	s
+        if (!pause)
+        {
+            ++count;
+            if (count == 20)
+            {
+                count = 0;
+                if (!tObj->moveDown(grid) && !freezeTetromino(tObj, tNext, grid))
+                {
+                    exitGame = true;
+                    break;
+                }
+            }
+        }
+        fore = GetForegroundWindow();
+    }  // while
 
-	gotoxy(0, wallHeight + 1);
-	if (exitGame)
-	{
-		std::println("{}", "Game Over");
-	}
-	while (_kbhit()) _getch(); // clear the keyboard input buffer
+    gotoxy(0, wallHeight + 1);
+    if (exitGame)
+    {
+        std::println("{}", "Game Over");
+    }
+    while (_kbhit()) _getch(); // clear the keyboard input buffer
 }
